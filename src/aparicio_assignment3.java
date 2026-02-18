a73cb6efaba029d06b9fade65ef277bf253ab6bc
KevnAp <apariciodkevin@Gmail.com>
2026-02-18 10:50:40 -0600

    built array to hold content and display contents of array

--- src/aparicio_assignment3.java
+++ src/aparicio_assignment3.java
@@ -5,6 +5,13 @@ public class aparicio_assignment3 {
 
         Scanner sc = new Scanner(System.in);
 
+        System.out.println("Type array size:");
+        int size = sc.nextInt();
 
+        int[] arr = new int[size];
+
+        for (int i = 0; i < size; i++){ // Prints out contents of array
+            System.out.print(arr [i] + "\t");
+        }
     }
 }
