62d09b6fc8dd020a01d3c5347425b7fd3a94ef9a
KevnAp <apariciodkevin@Gmail.com>
2026-02-18 22:08:42 -0600

    finalized program. Final commit

--- src/aparicio_assignment3.java
+++ src/aparicio_assignment3.java
@@ -22,8 +22,55 @@ public class aparicio_assignment3 {
         }
         // After sorting
         System.out.println("\n---After sorting in ascending order---");
-        for (int k = 0; k < size; k++){
-            System.out.print(arr [k] + "\t");
+        System.out.println("====Exchange Sort Algorithm====");
+        exchangeSort(arr);
+        for (int n = 0; n < arr.length; n++){
+            System.out.print(arr [n] + "\t");
+        }
+        System.out.println();
+
+        // Binary Search
+        binarySearch(arr);
+        sc.close();
+    }
+
+    private static void exchangeSort(int[] arr){
+        for (int l = 0; l < arr.length - 1; l++){
+
+            for (int m = l + 1; m < arr.length; m++){
+                if (arr[l] > arr[m]){
+                    int temp = arr[l];
+                    arr[l] = arr[m];
+                    arr[m] = temp;
+                }
+            }
+
+            for (int k = 0; k < arr.length; k++){
+                System.out.print(arr [k] + "\t");
+            }
+            System.out.println();
+        }
+    }
+
+    private static void binarySearch(int[] arr){
+        Scanner sc2 = new Scanner(System.in);
+
+        System.out.println("\nEnter the element to search for:");
+        int target = sc2.nextInt();
+        int low = 0;
+        int high = arr.length - 1;
+        int mid;
+
+        for (int o = 0; o < arr.length; o++){
+            mid = (low + high) / 2;
+            if (target == arr[mid]){
+                System.out.println("Search Found!!");
+                break;
+            } else if (target < arr[mid]) {
+                high = mid - 1;
+            } else {
+                low = mid + 1;
+            }
         }
     }
 }
